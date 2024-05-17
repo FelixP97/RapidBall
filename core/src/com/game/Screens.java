67833b94606cccad5bf2ee3e5a5ba7122d962ecf
FelixP97 <felixjosue105@gmail.com>
2024-05-16 22:15:10 -0600

    Se agrega ball

--- core/src/com/game/Screens.java
+++ core/src/com/game/Screens.java
@@ -8,13 +8,14 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
 
 public abstract class Screens extends InputAdapter implements Screen {
     public static final float SCREEN_WIDTH = 640;
     public static  final  float SCREEN_HEIGHT = 920;
 
-    public static  final  float WORLD_HEIGHT = 4.8f;
+    public static  final  float WORLD_HEIGHT = 11.5f;
     public static  final  float WORLD_WIDTH = 8f;
 
     public RapidBall game;
@@ -65,8 +66,10 @@ public abstract class Screens extends InputAdapter implements Screen {
 
     @Override
     public void resize(int width, int height) {
+        // Actualizar la vista del stage
+        stage.getViewport().update(width, height, true);
 
-        stage.getViewport().update(width,height,true);
+
 
     }
 
--- core/src/com/game/pantallas/Pantalla1.java
+++ core/src/com/game/pantallas/Pantalla1.java
@@ -1,8 +1,17 @@
 package com.game.pantallas;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.CircleShape;
+import com.badlogic.gdx.physics.box2d.EdgeShape;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.World;
+import com.game.Assets;
 import com.game.RapidBall;
 import com.game.Screens;
 
@@ -10,20 +19,98 @@ public class Pantalla1 extends Screens {
 
     Box2DDebugRenderer renderer;
     World oWorld;
+    TextureRegion ball;
     public Pantalla1(RapidBall game) {
         super(game);
         Vector2 gravity = new Vector2(0,-9.8f);
         oWorld = new World(gravity,true);
         renderer = new Box2DDebugRenderer();
+
+        ball = new TextureRegion(new Texture(Gdx.files.internal("data/ball.png")));
+
+        createFloor();
+        createBall();
+
+
+    }
+    private void createFloor(){
+        BodyDef bd = new BodyDef();
+        bd.position.set(0,0.5f);
+        bd.type = BodyDef.BodyType.StaticBody;
+
+        EdgeShape shape = new EdgeShape();
+        shape.set(0,0,WORLD_WIDTH,0);
+
+        FixtureDef fixDef= new FixtureDef();
+        fixDef.shape = shape;
+
+        Body oBody = oWorld.createBody(bd);
+        oBody.createFixture(fixDef);
+        shape.dispose();
+    }
+
+    private void createBall(){
+        BodyDef bd = new BodyDef();
+        bd.position.set(4f,11f);
+        bd.type = BodyDef.BodyType.DynamicBody;
+
+        CircleShape shape = new CircleShape();
+        shape.setRadius(.15f);
+
+        FixtureDef fixDef = new FixtureDef();
+        fixDef.shape = shape;
+        fixDef.density = 15;
+        fixDef.friction = .5f;
+        fixDef.restitution = .5f;
+
+        Body body = oWorld.createBody(bd);
+        body.createFixture(fixDef);
+        shape.dispose();
     }
 
     @Override
     public void draw(float delta) {
+        oCamUI.update();
+        spriteBatch.setProjectionMatrix(oCamUI.combined);
 
+        spriteBatch.begin();
+        Assets.font.draw(spriteBatch,"Fps:"+ Gdx.graphics.getFramesPerSecond(),0,20);
+        spriteBatch.end();
+
+        oCamBox2D.update();
+        spriteBatch.setProjectionMatrix(oCamBox2D.combined);
+        spriteBatch.begin();
+        drawGameObjects();
+        spriteBatch.end();
+
+        renderer.render(oWorld, oCamBox2D.combined);
     }
+
+    private void drawGameObjects(){
+        
+    }
+
 
     @Override
     public void update(float delta) {
+        oWorld.step(delta,8,6);
+    }
 
+    static public class GameObject{
+        static final int Ball = 0;
+        static final int Box = 1;
+        final int type;
+        float angleDeg;
+        Vector2 position;
+
+        public GameObject(float x,float y,int type) {
+            position = new Vector2(x,y);
+            this.type = type;
+        }
+        public void update(Body body){
+            position.x = body.getPosition().x;
+            position.y = body.getPosition().y;
+            angleDeg = (float) Math.toDegrees(body.getAngle());
+        }
     }
 }
